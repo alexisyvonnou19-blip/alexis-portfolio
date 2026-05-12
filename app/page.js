4c8cf13c38e18e093882fa499880449a10c2e2f4
alexisyvonnou19-blip <alexisyvonnou19@gmail.com>
2026-05-12 14:27:42 +0200

    Remove unused homepage state

--- app/page.js
+++ app/page.js
@@ -2,12 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
-import {
-  ArrowRight,
-  Mail,
-  ChevronUp,
-} from "lucide-react";
+import { ArrowRight, Mail, ChevronUp } from "lucide-react";
 import { motion } from "framer-motion";
 
 const photographyCategories = [
