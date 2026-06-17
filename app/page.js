d5c0e0cfda8e3a101e9a37539986a163e8798ca4
alexisyvonnou19-blip <alexisyvonnou19@gmail.com>
2026-06-17 18:56:39 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -770,7 +770,7 @@ export default function AlexisYvonnouHomepage() {
             </div>
           </div>
         </div>
-      </section> 
+      </section>
 
       <AnimatePresence>
         {showBackToTop && (
