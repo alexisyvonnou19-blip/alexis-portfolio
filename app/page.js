937ed49f404e0afc9bfb2f9504779a6f9cda5d38
alexisyvonnou19-blip <alexisyvonnou19@gmail.com>
2026-06-17 11:45:01 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -68,7 +68,7 @@ function PhotoTransition({ image, label }) {
             alt={label}
             fill
             sizes="100vw"
-            className="object-cover opacity-85"
+            className="object-cover opacity-100"
           />
 
           <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent" />
